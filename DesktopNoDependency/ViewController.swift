2881f7ce246433be57f8deb4ebbbfdb46236af64
Nghia Tran <vinhnghiatran@gmail.com>
2018-10-16 20:10:41 +0700

    Initial Commit

---
+++ DesktopNoDependency/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DesktopNoDependency
+//
+//  Created by Nghia Tran on 10/16/18.
+//  Copyright © 2018 com.nsproxy.proxy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
